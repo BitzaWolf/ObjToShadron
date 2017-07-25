7ecd2be697c72db59e7f752d451207dffd973517
Anthony 'Bitzawolf' Pepe <bitzah@gmail.com>
2017-07-24 19:53:57 -0700

    Wrote out overview of tasks.

---
+++ ObjToShadron.py
@@ -0,0 +1,9 @@
+# 0) Read in all the verticies. 1-based index.
+#    Read in all tex coords
+#    Read in all normal coords
+#    Read in faces
+# 1) Determine if quads or trigs, 4 or 3 verts per face
+#    Write num of verts
+# 2) Write vertex coord function, based on faces and verts
+#    Write vertex texcoord function
+#    Write vertex normal function
